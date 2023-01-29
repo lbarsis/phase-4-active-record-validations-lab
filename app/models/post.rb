e3acd85d063af45f3bb55b6bf097b256be9dad42
Levi Barsis <lbarsis@gmail.com>
2023-01-29 11:37:50 -0800

    Done with assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
+  validate :clickbate
+
+  def clickbate
+    if title == 'True Facts'
+      self.errors.add :title, "Choose a different title!"
+    end
+  end
 end
